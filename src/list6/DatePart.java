9663ee596a7062685a4054d0e1a1dd4fdd2009e5
Platina1337 <bbd3372005@gmail.com>
2023-02-17 16:16:51 +0300

    1

---
+++ src/list6/DatePart.java
@@ -0,0 +1,36 @@
+package list6;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class DatePart {
+    private Date fromDate = null;
+    private SimpleDateFormat formatter = null;
+    public DatePart(Date anyDate) {
+        fromDate = anyDate;
+        fromDate = new SimpleDateFormat("EEE MMM dd hh:mm:ss yyyy" , Locale.getDefault()).get2DigitYearStart();
+
+    }
+            public int getDay() {
+        formatter.applyPattern("d");
+        return Integer.parseInt(formatter.format(fromDate));
+    }
+    public int getMonth() {
+        formatter.applyPattern("M");
+        return Integer.parseInt(formatter.format(fromDate));
+    }
+    public int getYear() {
+        formatter.applyPattern("y");
+        return Integer.parseInt(formatter.format(fromDate));
+    }
+    public int hetHour() {
+        formatter.applyPattern("h");
+        return Integer.parseInt(formatter.format(fromDate));
+    }
+    public int getMinute() {
+        formatter.applyPattern("m");
+        return Integer.parseInt(formatter.format(fromDate));
+    }
+
+}
